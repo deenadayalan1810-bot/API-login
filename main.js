7b2692568c4e34982b9d9fe0aeb81731dd57bfe3
deenadayalan1810 <deenadayalan1810@gmail.com>
2025-12-14 20:04:07 +0530

    updated backend api project

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 import express from "express"
+import cors from "cors"
 import iplRoutes from "./routes/Ipl.route.js"
 import connectDB from "./lib/db.js"
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 connectDB();
 
